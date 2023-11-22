7eda2555141d9af09e0b719ab983adf8d73ae8da
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-11-22 18:14:04 +0300

    Major adjustments to the toggling classes

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+var page1 = document.querySelector(".container");
+var page2 = document.querySelector(".subscribe");
 var email = document.getElementById("email-input");
 var emailExample =/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/;
 var form = document.getElementById("form");
 var labelErr = document.querySelector(".error")
 
 form.addEventListener("submit", (e)=>{
-    e.preventDefault();
-
+    if(!validateEmail()){
+        e.preventDefault();
+    }
     validateEmail();
 })
-
 function validateEmail(){
     const emailValue = email.value.trim();
     if(emailValue === ""){
@@ -18,6 +20,8 @@ function validateEmail(){
             labelErr.classList.add("showing");
         }else{
             labelErr.classList.remove("showing");
+            page1.classList.add("secret");
+            page2.classList.remove("secret");
         }
     }
 }
